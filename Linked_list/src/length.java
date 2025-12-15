0f426d92fbe7383fd5935a97d89adcce64fed2dc
akanksha3604 <singhakanksha3604@gmail.com>
2025-12-16 01:13:15 +0530

    Added Length of LL program

--- Linked_list/src/length.java
+++ Linked_list/src/length.java
@@ -1,3 +1,31 @@
-public class length {
 
+public class length {
+    public static class Node
+    {
+        int data;
+        Node next;
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static void main(String args[]) {
+        Node head = new Node(1);
+        head.next = new Node(3);
+        head.next.next = new Node(1);
+        head.next.next.next = new Node(2);
+        head.next.next.next.next = new Node(2);
+        System.out.println("Count of nodes are:" + countNodes(head));
+    }
+
+    public static int countNodes(Node head) {
+        int count = 0;
+        for (Node curr = head; curr != null; curr = curr.next) {
+            count++;
+        }
+        return count;
+    }
 }
+
+
