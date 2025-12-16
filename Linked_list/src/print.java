60d91b60b3d62d9da603050f59e2e48f2a3f539e
Aryan Sahu <aryansahu0325@gmail.com>
2025-12-16 09:39:07 +0530

    Added Print LL program

---
+++ Linked_list/src/print.java
@@ -0,0 +1,30 @@
+
+public class print {
+    public static class Node
+    {
+        int data;
+        Node next;
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static void main(String args[]) {
+        Node head = new Node(1);
+        head.next = new Node(3);
+        head.next.next = new Node(1);
+        head.next.next.next = new Node(2);
+        head.next.next.next.next = new Node(2);
+        printNodes(head);
+    }
+
+    public static void printNodes(Node head) {
+        for (Node curr = head; curr != null; curr = curr.next) {
+            System.out.print(curr.data+"->");
+        }
+        System.out.println("null");
+    }
+}
+
+
