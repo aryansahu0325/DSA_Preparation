c4105c56355e60b254e80d4fe1323aa7475f84f0
Aryan Sahu <aryansahu0325@gmail.com>
2025-12-26 18:01:26 +0530

    Position and End Inserted Added

--- Linked_list/src/insert_end.java
+++ Linked_list/src/insert_end.java
@@ -1,2 +1,57 @@
-public class insert_end {
+import java.util.*;
+public class insert_end
+{
+    public static class Node
+    {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        Node head=new Node(2);
+        head.next=new Node(6);
+        head.next.next=new Node(5);
+        head.next.next.next=new Node(1);
+        head.next.next.next.next=new Node(8);
+        System.out.println("Original List");
+        print_list(head);
+        System.out.println("Enter the value to inserted");
+        int n=sc.nextInt();
+        insert_node_end(head,n);
+        System.out.println("Updated list");
+        print_list(head);
+    }
+    public static void print_list(Node head)
+    {
+        if(head==null)
+        {
+            return;
+        }
+        System.out.print(head.data);
+        if(head.next!=null)
+            System.out.print("->");
+        print_list(head.next);
+        System.out.println();
+    }
+    public static void insert_node_end(Node head, int n)
+    {
+        Node newNode=new Node(n);
+        if(head==null){
+            return;
+        }
+        for(Node temp=head;temp!=null;temp=temp.next)
+        {
+            if(temp.next==null) {
+                temp.next = newNode;
+                return;
+            }
+        }
+    }
 }
+
--- Linked_list/src/insert_pos.java
+++ Linked_list/src/insert_pos.java
@@ -1,2 +1,53 @@
+import java.util.*;
 public class insert_pos {
+    public static class Node
+    {
+        int data;
+        Node next;
+        Node(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value to be inserted");
+        int n=sc.nextInt();
+        System.out.println("Enter the position");
+        int p=sc.nextInt();
+        Node head=new Node(2);
+        head.next=new Node(3);
+        head.next.next=new Node(4);
+        head.next.next.next=new Node(6);
+        head.next.next.next.next=new Node(7);
+        print(head);
+        insertpos(head,n,p);
+        print(head);
+    }
+    public static Node insertpos(Node head,int n,int p)
+    {
+        Node curr=head;
+        for(int i=1;i<p-1;i++)
+        {
+            curr=curr.next;
+        }
+        Node newNode=new Node(n);
+        newNode.next=curr.next;
+        curr.next=newNode;
+        return head;
+    }
+    public static void print(Node head)
+    {
+        Node temp=head;
+        for(temp=head;temp!=null;temp=temp.next)
+        {
+            System.out.print(temp.data);
+            if(temp.next!=null)
+            {
+                System.out.print("->");
+            }
+        }
+    }
 }
