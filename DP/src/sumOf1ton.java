9cd1d08b93142b1ae8f312d3c4b6b6f288bb0215
Aryan Sahu <sahuaryan0325@gmail.com>
2025-12-27 10:50:37 +0530

    Added Sum of 1 to n Recursive Program

---
+++ DP/src/sumOf1ton.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class sumOf1ton
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+        System.out.println("Sum of Digit of No."+sum1ton(n));
+    }
+    public static int sum1ton(int n)
+    {
+        int sum=0;
+        if(n==0){
+            return sum;
+        }
+        sum+=n;
+        return n+sum1ton(n-1);
+
+    }
+}
